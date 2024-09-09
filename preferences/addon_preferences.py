6d504616d84e87236a86b905974f4dbd40225b9a
MekuMaki <sage2014@gmail.com>
2024-09-08 23:04:49 -0500

    Fixed Error
    ... why does the ARMATURE_OT_AppendVariantToYAML break...? it doesn't break on the .zips that Gerula sent me wtfffff.
    
    I think this is the only thing that is directly affecting Gerula's stuff so I'm going to comment them out to test...

--- preferences/addon_preferences.py
+++ preferences/addon_preferences.py
@@ -1,18 +1,18 @@
 import bpy  # type: ignore
 
-# Addon Preferences class
-class MyAddonPreferences(bpy.types.AddonPreferences):
-    bl_idname = __name__
+# # Addon Preferences class
+# class MyAddonPreferences(bpy.types.AddonPreferences):
+#     bl_idname = __name__
 
-    # Add a toggle property to control the "Development" panel visibility
-    enable_development_panel: bpy.props.BoolProperty(
-        name="Enable Development Panel",
-        description="Show or hide the Development panel in the N-panel",
-        default=True
-    ) # type: ignore
+#     # Add a toggle property to control the "Development" panel visibility
+#     enable_development_panel: bpy.props.BoolProperty(
+#         name="Enable Development Panel",
+#         description="Show or hide the Development panel in the N-panel",
+#         default=True
+#     ) # type: ignore
 
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(self, "enable_development_panel")
+#     def draw(self, context):
+#         layout = self.layout
+#         layout.prop(self, "enable_development_panel")
 
-        return {'FINISHED'}
+#         return {'FINISHED'}
